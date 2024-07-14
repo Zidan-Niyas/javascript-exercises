b8c973189127d8841ff83f97b63a77cc257f9315
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-14 18:47:28 +0530

    completed 5th exercise (sumAll)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    else if(start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    else if(typeof(start) != 'number' || typeof(end) != 'number') {
+        return 'ERROR';
+    }
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
