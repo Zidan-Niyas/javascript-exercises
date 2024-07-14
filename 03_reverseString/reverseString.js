c4ac44f2fe29882d559b8d13e1dff429e050ba97
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-14 12:55:32 +0530

    completed 3rd exercise (reverseString)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const split = string.split("");
+    let newStr = '';
+    for(let i=split.length-1; i>=0; i--) {
+        newStr = newStr + split[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
