d79b5f75d7e38a63821a037e56537c7d7a5cf29c
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-14 19:57:16 +0530

    completed 7th exercise (temp convertion)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  const celsius = ((far - 32) * (5/9)).toFixed(1);
+  return Math.round(celsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const fahrenheit = ((cel * (9/5)) + 32).toFixed(1);
+  return Math.round(fahrenheit*10)/10; 
 };
 
 // Do not edit below this line
