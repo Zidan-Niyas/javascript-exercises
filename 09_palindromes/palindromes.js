187c8e3091021b235d752c818517a5b1e8ec1fc5
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-23 13:02:10 +0530

    completed 09_palindromes (checked solution)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+    const reversedString = cleanedString
+    .split('')
+    .reverse()
+    .join('');
+
+    return reversedString === cleanedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
