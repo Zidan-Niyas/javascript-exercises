4378b4da31c4598e8b5e173ab8955197548bb02f
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-23 15:59:53 +0530

    completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 1;
+    let b = 1;
+    if(num < 0) {
+        return "OOPS";
+    }
+    else if(num == 0) {
+        return 0;
+    }
+    else if(num == 1) {
+        return a;
+    }
+    else if(num == 2) {
+        return b;
+    }
+    else {
+        for(i=3; i<=num; i++) {
+            let current = a + b; // temp = 2
+            a = b;  // a = 3
+            b = current; // b = 2 + 3 = 5
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
